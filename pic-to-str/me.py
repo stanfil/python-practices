266f48c2cc961b1837e83b63aae5eae0fdbc601b
shaoshuai <352070351@qq.com>
2019-11-21 10:36:16 +0800

    pic-to-str: me.py

--- pic-to-str/me.py
+++ pic-to-str/me.py
@@ -1,6 +1,45 @@
 from PIL import Image
+import argparse
 
-im1 = Image.open("D:/桌面/壁纸/1920x1080.jpg").convert('RGBA').resize((1920, 1080))
-im2 = Image.open("D:/桌面/壁纸/下载 (6).jpg").convert('RGBA').resize((1920, 1080))
-out = Image.alpha_composite(im1, im2)
-im2.show()
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file')
+parser.add_argument('-o', '--output')
+parser.add_argument('--width', type=int, default=80)
+parser.add_argument('--height', type=int, default=80)
+
+args = parser.parse_args()
+
+FILE = args.file
+OUTPUT = args.output
+WIDTH = args.width
+HEIGHT = args.height
+
+chars = '$ '
+
+
+def getchar(r, g, b, a=256):
+    if a == 0:
+        return ' '
+
+    gray = int(0.2126 * r + 0.7152 * g + 0.0722 * b)
+    step = 256 / len(chars)
+    return chars[int(gray / step)]
+
+
+if __name__ == '__main__':
+    img = Image.open(FILE)
+    img = img.resize((WIDTH, HEIGHT), Image.NEAREST)
+
+    text = ''
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            text += getchar(*img.getpixel((j, i)))
+        text += '\n'
+
+    if OUTPUT:
+        with open(OUTPUT, 'w') as f:
+            f.write(text)
+    else:
+        with open('output.txt', 'w') as f:
+            f.write(text)
